a76d80e23d562a79ccbfb0d7462c7c886cdaefa4
Александр <@DESKTOP-L9V170T>
2025-05-24 10:06:45 +0300

    Добавьте файлы проекта.

---
+++ DigitTwin.Lib.Contracts/DTO/CountItemDto.cs
@@ -0,0 +1,19 @@
+﻿namespace DigitTwin.Lib.Contracts.DTO
+{
+    /// <summary>
+    /// Количественный вывод элементов
+    /// </summary>
+    /// <typeparam name="TItem">Тип данных</typeparam>
+    public class CountItemDto<TItem>
+    {
+        /// <summary>
+        /// Количество элементов
+        /// </summary>
+        public int Count { get; set; } = 0;
+
+        /// <summary>
+        /// Элементы
+        /// </summary>
+        public TItem[]? Items { get; set; }
+    }
+}
