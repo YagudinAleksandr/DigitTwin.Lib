cc29d6625d3d427b4dbf0cfdc15048dc74a00de7
Александр <@DESKTOP-L9V170T>
2025-05-24 14:21:34 +0300

    Добавлены все геообъекты

---
+++ DigitTwin.Lib.Geo.Abstractions/GeoObjectInitialPoint.cs
@@ -0,0 +1,15 @@
+﻿namespace DigitTwin.Lib.Geo.Abstractions
+{
+    /// <summary>
+    /// Стартовая точка
+    /// </summary>
+    public class GeoObjectInitialPoint : GeoObject
+    {
+        public override GeoTypeEnum Type => GeoTypeEnum.INITIAL_POINT;
+
+        /// <summary>
+        /// Начальные координаты
+        /// </summary>
+        public GeoCordinates Position { get; set; } = new GeoCordinates();
+    }
+}
---
+++ DigitTwin.Lib.Geo.Abstractions/GeoObjectPolygon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace DigitTwin.Lib.Geo.Abstractions
+{
+    /// <summary>
+    /// Полигон
+    /// </summary>
+    public class GeoObjectPolygon : GeoObject
+    {
+        public override GeoTypeEnum Type => GeoTypeEnum.POLYGON;
+
+        /// <summary>
+        /// Координаты
+        /// </summary>
+        public List<List<GeoCordinates>> Cordinates { get; set; } = new List<List<GeoCordinates>>();
+    }
+}
