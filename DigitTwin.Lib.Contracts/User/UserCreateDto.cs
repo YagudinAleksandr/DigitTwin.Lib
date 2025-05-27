78e6c5761d31c6c9f8101806be8b044faa5f2c81
Александр <@DESKTOP-L9V170T>
2025-05-27 22:45:36 +0300

    Добавлены модели

---
+++ DigitTwin.Lib.Contracts/User/UserCreateDto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DigitTwin.Lib.Contracts.User
+{
+    /// <summary>
+    /// Создание пользователя
+    /// </summary>
+    public class UserCreateDto
+    {
+        /// <summary>
+        /// Тип пользователя
+        /// </summary>
+        public int Type { get; set; }
+
+        /// <summary>
+        /// ФИО
+        /// </summary>
+        public string Name { get; set; } = null!;
+
+        /// <summary>
+        /// E-mail
+        /// </summary>
+        public string Email { get; set; } = null!;
+
+        /// <summary>
+        /// Пароль
+        /// </summary>
+        public string Password { get; set; } = null!;
+
+        /// <summary>
+        /// Статус активности
+        /// </summary>
+        public int Status { get; set; }
+
+        /// <summary>
+        /// ИД организации
+        /// </summary>
+        public Guid? OrganizationId { get; set; }
+    }
+}
