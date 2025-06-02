3ba499ccccaae395c00bad35de258669b7e9f7fb
Александр <@DESKTOP-L9V170T>
2025-06-02 21:42:19 +0300

    Добавлен библиотека валидатора

---
+++ DigitTwin.Lib.Validator/ValidationRule.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace DigitTwin.Lib.Validator
+{
+    /// <summary>
+    /// Правила валидации
+    /// </summary>
+    /// <typeparam name="TModel"></typeparam>
+    public abstract class ValidationRule<TModel>
+    {
+        /// <summary>
+        /// Получение списка валидации
+        /// </summary>
+        /// <param name="obj">Модель</param>
+        /// <returns>Список ошибок</returns>
+        public abstract IEnumerable<string> Validate(TModel obj);
+    }
+}
